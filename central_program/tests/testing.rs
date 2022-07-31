4a515792ccb69308bdd68d7746bd471a49ab85c0
psaunderualberta <psaunder@ualberta.ca>
2022-07-31 13:05:41 -0400

    Adding tests for 'unset'

--- central_program/tests/testing.rs
+++ central_program/tests/testing.rs
@@ -269,14 +269,104 @@ fn test_play_on_opponents_spot() -> io::Result<()> {
 // test different win conditions
 // test different loss conditions
 
-// test unset
+/**
+ * LET THE TESTING OF 'UNSET' BEGIN
+ * NOTE: These tests assume that 'seto' and 'sety' work as intended
+ */
 // test clearing
+#[test]
+fn test_unset_clears_correctly() -> io::Result<()> {
+    let mut bot = get_bot()?;
+
+    let bot_in = bot.stdin.as_mut().unwrap();
+    write!(bot_in, "sety a1\n")?;
+    write!(bot_in, "unset a1\n")?;
+    write!(bot_in, "show_board\n")?;
+    let output = get_command_output(&mut bot, 2);
+
+    assert_eq!(
+        output.as_str().trim_start(),
+        ". . . . . . . . 
+ . . . . . . . . 
+  . . . . . . . . 
+   . . . . . . . . 
+    . . . . . . . . 
+     . . . . . . . . 
+      . . . . . . . . 
+       . . . . . . . . 
+= 
+"
+    );
+
+    shutdown_bot(&mut bot)?;
+    Ok(())
+}
+
 // test that other cells are unaffected
+#[test]
+fn test_unset_leaves_other_cells() -> io::Result<()> {
+    let mut bot = get_bot()?;
+
+    let bot_in = bot.stdin.as_mut().unwrap();
+    let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    for letter in letters {
+        for digit in 1..=8 {
+            write!(bot_in, "{} {}{}\n", "sety", letter, digit.to_string().as_str())?;
+        }
+    }
+    write!(bot_in, "unset a1\n")?;
+    write!(bot_in, "show_board\n")?;
+    let output = get_command_output(&mut bot, (letters.len() as i32) * 8 + 1);
+
+    assert_eq!(
+        output.as_str().trim_start(),
+        ". W W W W W W W 
+ W W W W W W W W 
+  W W W W W W W W 
+   W W W W W W W W 
+    W W W W W W W W 
+     W W W W W W W W 
+      W W W W W W W W 
+       W W W W W W W W 
+= 
+"
+    );
+
+    shutdown_bot(&mut bot)?;
+    Ok(())
+}
+
 // test clearing empty cell
+#[test]
+fn test_unset_on_empty_cell() -> io::Result<()> {
+    let mut bot = get_bot()?;
+
+    let bot_in = bot.stdin.as_mut().unwrap();
+    write!(bot_in, "unset a1\n")?;
+    write!(bot_in, "show_board\n")?;
+    let output = get_command_output(&mut bot, 1);
+
+    assert_eq!(
+        output.as_str().trim_start(),
+        ". . . . . . . . 
+ . . . . . . . . 
+  . . . . . . . . 
+   . . . . . . . . 
+    . . . . . . . . 
+     . . . . . . . . 
+      . . . . . . . . 
+       . . . . . . . . 
+= 
+"
+    );
+
+    shutdown_bot(&mut bot)?;
+    Ok(())
+}
 
 /*
-    LET THE TESTING OF INIT_BOARD COMMENCE
-*/
+ *LET THE TESTING OF INIT_BOARD COMMENCE
+ */
 #[test]
 fn test_init_board_3() -> io::Result<()> {
     let mut bot = get_bot()?;
