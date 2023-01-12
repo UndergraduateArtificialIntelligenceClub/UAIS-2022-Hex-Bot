4e97dfd1a54efaa30f41d2b3fefcc77a9229eccf
Akemi Izuko <akemi@noway.moe>
2023-01-12 00:27:37 -0700

    Update: Finalize tui visuals

--- central_program/src/display_interface.rs
+++ central_program/src/display_interface.rs
@@ -58,16 +58,18 @@ fn main() -> Try<()> {
 struct App {
     board: Board,
     size: usize,
-    status_msg: String,
+    messages: Vec<String>,
     color: Tile,
 }
 
 impl App {
     pub fn new(color: Tile) -> Self {
+        let default_size = 6;
+
         Self {
-            board: Board::new(6),
-            size: 6,
-            status_msg: "Starting game...".to_string(),
+            board: Board::new(default_size),
+            size: default_size,
+            messages: vec!["Starting game...".to_string(), "Starting game...".to_string()],
             color,
         }
     }
@@ -83,19 +85,19 @@ impl App {
         let stdin = io::stdin();
         let mut lines = stdin.lock().lines().map(|s| s.expect("Failed to read line from stdin"));
 
-
         terminal.draw(|f| self.tui(f))?;
+        self.get_next_click(terminal).expect("Failed to get crossterm event");
 
         for ref line in lines {
             if re_init.is_match(line) {
                 let caps = re_init.captures(line).unwrap();
                 self.size = caps.get(1).unwrap().as_str().parse()?;
                 self.board = Board::new(self.size as u8);
-                self.status_msg = format!("Created new board of size {s}x{s}", s = self.size);
+                self.messages.push(format!("Created new board of size {s}x{s}", s = self.size));
             } else if re_make_move.is_match(line) {
                 let mut stdout = io::stdout().lock();
-                let reply = self.get_next_click().expect("Failed to get crossterm event");
-                writeln!(&mut stdout, "{reply}")?;
+                //let reply = self.get_next_click().expect("Failed to get crossterm event");
+                //writeln!(&mut stdout, "{reply}")?;
             } else if re_seto.is_match(line) {
                 todo!();
             } else if re_sety.is_match(line) {
@@ -114,16 +116,20 @@ impl App {
         Ok(())
     }
 
-    fn get_next_click(&mut self) -> Try<String> {
+    fn get_next_click<B: Backend>(&mut self, terminal: &mut Terminal<B>) -> Try<String> {
         loop {
             if let Event::Mouse(mouse) = crossterm::event::read()? {
-                let row = ((mouse.row as usize - 3 - 1) / 2) + 1;
-                let col = ((mouse.column as usize - row + 1 - 3 - 1) / 2) + 1;
+                self.messages.push(format!("({}, {})", mouse.row, mouse.column));
+                terminal.draw(|f| self.tui(f))?;
 
-                if row <= self.size && col <= self.size {
-                    let c = char::from_u32((row + 97) as u32).unwrap();
-                    return Ok(format!("{}{}", c, col));
-                }
+                //let row = ((mouse.row as usize - 3 - 1) / 2) + 1;
+                //let col = ((mouse.column as usize - row + 1 - 3 - 1) / 2) + 1;
+
+
+                //if row <= self.size && col <= self.size {
+                //    let c = char::from_u32((row + 97) as u32).unwrap();
+                //    return Ok(format!("{}{}", c, col));
+                //}
             }
         }
     }
@@ -134,7 +140,7 @@ impl App {
             .margin(1)
             .horizontal_margin(2)
             .constraints([
-                Constraint::Length((self.size * 6 - 1) as u16),
+                Constraint::Length((self.size * 5 + 2) as u16),
                 Constraint::Min(1),
             ].as_ref())
             .split(f.size());
@@ -142,10 +148,20 @@ impl App {
         outer[1].x += 3 * self.size as u16;
         outer[1].width -= 3 * self.size as u16;
 
+        let left_side = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(1), Constraint::Min(1)].as_ref())
+            .split(outer[0]);
+
+        let horizontal_board = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Length(2), Constraint::Min(1)].as_ref())
+            .split(left_side[1]);
+
         let board_rows = Layout::default()
             .direction(Direction::Vertical)
             .constraints(vec![Constraint::Length(3); self.size + 1].as_ref())
-            .split(outer[0]);
+            .split(horizontal_board[1]);
 
         let tile_block = Block::default()
             .borders(Borders::ALL)
@@ -154,7 +170,7 @@ impl App {
         for i in 0..self.size {
             let cols = Layout::default()
                 .direction(Direction::Horizontal)
-                .constraints(vec![Constraint::Length(5); self.size + 1].as_ref())
+                .constraints(vec![Constraint::Length(5); self.size].as_ref())
                 .split(board_rows[i]);
 
             for mut col in cols {
@@ -167,6 +183,38 @@ impl App {
             }
         }
 
-        f.render_widget(Paragraph::new(self.status_msg.clone()).block(tile_block), outer[1]);
+        let mut col_labels: Vec<_> = (1..=(self.size + 1)).map(|n| format!("    {n}")).collect();
+        eprintln!("Col length: {}", col_labels.len());
+        eprintln!("Should be: {}", self.size);
+
+        let para = Paragraph::new(col_labels.join(""));
+        f.render_widget(para, left_side[0]);
+
+        let mut row_labels = (1..=self.size)
+            .map(|n| char::from_u32(n as u32 + 96).unwrap())
+            .map(|c| ListItem::new(c.to_string()))
+            .collect::<Vec<ListItem>>();
+
+        for i in 0..row_labels.len() {
+            row_labels.insert(i * 3 + 1, ListItem::new(" ".to_string()));
+            row_labels.insert(i * 3 + 1, ListItem::new(" ".to_string()));
+        }
+
+        row_labels.insert(0, ListItem::new(" ".to_string()));
+
+        f.render_widget(List::new(row_labels), horizontal_board[0]);
+
+        let list = List::new(self.messages
+                .iter()
+                .rev()
+                .map(|s| ListItem::new(s.clone()))
+                .collect::<Vec<ListItem>>())
+            .start_corner(Corner::BottomLeft);
+
+        f.render_widget(
+            list.block(Block::default().borders(Borders::ALL).title("Play Log")),
+            outer[1]);
+
+        //f.render_widget(Block::default().borders(Borders::ALL), outer[0]);
     }
 }
