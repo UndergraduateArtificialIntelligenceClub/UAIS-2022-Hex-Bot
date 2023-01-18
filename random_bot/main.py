47dd6fcc99d957ca7d7c5d7f28db36f646e5ed0f
psaunderualberta <psaunder@ualberta.ca>
2023-01-18 11:59:22 -0700

    Updated random_bot to include a help message

--- random_bot/main.py
+++ random_bot/main.py
@@ -16,13 +16,32 @@ def main():
     color = WHITE if args.color == "white" else BLACK
     bot = RandomHexBot(color)
 
+    help_items = [
+        ["Command", "Example", "Description"],
+        ["init_board {digit}", "init_board 8", "Tells the bot to reset the game to an empty board w/ side length digit"],
+        ["show_board", "show_board", "Prints the board to stdout. Used for internal testing"],
+        ["make_move", "make_move", "Asks the bot to give their move, based on the current board"],
+        ["seto {}", "seto a1", "Tells the bot about a move for the other bot"],
+        ["sety {}", "sety a1", "Tells the bot to play a move for itself"],
+        ["swap", "swap", "Uses the opening \"swap\" move in Hex"],
+        ["unset {}", "unset a1", "Tells the bot to set a tile as unused"],
+        ["check_win", "check_win", "Tells the bot to check if the game is over. Returns 1 if itself has won, -1 if the opponent has won, 0 if the game has not terminated"],
+        ["quit", "quit", "The game is over"]
+    ]
+
+    help_response = "Cmd not recognized. Please refer to known commands below:"
+
     cmd = get_cmd()
     while cmd[0] != "quit":
         try:
             getattr(bot, cmd[0])
             bot.run_command(cmd)
         except AttributeError:
-            print("Cmd not recognized. Please refer to known commands.")
+            print(help_response)
+            for command, ex, desc in help_items:
+                print("{:30}{:30}{}".format(command, ex, desc))
+
+            print("\nNote that draws are impossible in hex, so no response for a draw is required")
 
         cmd = get_cmd()
     return
